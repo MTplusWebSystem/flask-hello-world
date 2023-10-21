e0b24b19a4c84dbb4a396ba87e3dcb032b4c2e37
MTplusWebSystem <MTplusWebSystem@gmail.com>
2023-10-20 23:17:50 -0400

    v0.0.4

--- api/index.py
+++ api/index.py
@@ -1,15 +1,34 @@
 from flask import Flask, request, jsonify
 import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/glmod/http=<path:url>', methods=['GET'])
-def intermediate(url):
+@app.route('/gl/url=<path:url>', methods=['GET'])
+def gl(url):
     full_url = f'http://{url}'
     try:
         response = requests.get(full_url)
         response_json = response.json()
         return jsonify(response_json), response.status_code
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/cg4/url=<path:url>', methods=['GET','POST'])
+def fourg(url):
+    try:
+        full_url = f'http://{url}'
+        req_data = request.get_json()
+        user = req_data.get("user")
+            
+        headers = {"Content-Type": "application/json"}
+        data = {
+            "user": user
+        }
+        json_data = json.dumps(data)
+        response = requests.post(full_url, data=json_data, headers=headers)
+        
+        return jsonify(response.json()), response.status_code
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
